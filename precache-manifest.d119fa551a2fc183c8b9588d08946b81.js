f9cbc3ff4b77ec5f77fb0c34f08216ae247749ea
milostrifunovic1 <milos.trifunovic.92@gmail.com>
2019-06-13 01:16:17 +0200

    Updates

---
+++ precache-manifest.d119fa551a2fc183c8b9588d08946b81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a301262d3392f21c58db62767f2db5",
+    "url": "/gittest/index.html"
+  },
+  {
+    "revision": "99429a8074b51c63fa9f",
+    "url": "/gittest/static/css/main.684b5bc1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gittest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "99429a8074b51c63fa9f",
+    "url": "/gittest/static/js/main.3d48bb5f.chunk.js"
+  },
+  {
+    "revision": "dbc0910263c1a1419a27",
+    "url": "/gittest/static/js/runtime~main.8a319a05.js"
+  },
+  {
+    "revision": "ffc4acb8c68be6f9925461e5905ff723",
+    "url": "/gittest/static/media/FC_Red_Star_Belgrade.ffc4acb8.png"
+  }
+]);
